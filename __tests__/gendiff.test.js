35323502ed695c9a3edaebca583e18da97085aed
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-09-26 12:33:42 +0600

    added nested json diff 2

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,8 +4,7 @@ test('should return as in the task: json format', () => {
   const file1 = 'file1.json';
   const file2 = 'file2.json';
 
-  const expectedResult = '{\n  - follow : false\n    host : hexlet.io\n  - proxy : 123.234.53.22\n  - timeout : 50\n  + timeout : 20\n  + verbose : true\n}';
-  console.log(expectedResult);
+  const expectedResult = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
   expect(genDiff(file1, file2)).toEqual(expectedResult);
 });
 
@@ -13,7 +12,6 @@ test('should return as in the task: yml format', () => {
   const file1 = 'file1.yml';
   const file2 = 'file2.yml';
 
-  const expectedResult = '{\n  - follow : false\n    host : hexlet.io\n  - proxy : 123.234.53.22\n  - timeout : 50\n  + timeout : 20\n  + verbose : true\n}';
-  console.log(expectedResult);
+  const expectedResult = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
   expect(genDiff(file1, file2)).toEqual(expectedResult);
 });
