aa11ee630732d9c6d8b6758e076436af23193a53
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-10-02 16:18:38 +0600

    ref plain

--- src/plain.js
+++ src/plain.js
@@ -1,47 +1,41 @@
 import _ from 'lodash';
 
-const stringify = (key, after, path) => {
-    if (!_.isObject(after)) {
-        const value = path;
-        return `Property ${value}'${key}' was added with value: '${after}'`;
-    }
-    return `Property '${key}' was added with value: [complex value]`;
-};
-const stringifyUpdated = (key, after, before) => {
-    if (_.isObject(before)) {
-        return `Property '${key}' was updated. From [complex value] to '${after}'`;
-    }
-    return `Property '${key}' was updated. From '${before}' to '${after}'`;
-};
-const stringifyRemoved = (key) => {
-    return `Property '${key}' was removed`;
+const stringify = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+
+  return value;
 };
 
 const plain = (tree) => {
-    const iter = (nestedTree, path) => {
-        const lines = nestedTree.flatMap((node) => {
-            const {
-                type, key, after, before, children,
-              } = node;
-              if (_.has(node, key) && _.has(node, after)) {
-                  path += key;
-              } 
-              if (type === 'added') {
-                  return stringify(key, after, path);
-              }
-              if (type === 'changed') {
-                  return iter(children, path);
-              }
-              if (type === 'updated') {
-                  return stringifyUpdated(key, after, before);
-              }
-              if (type == 'removed') {
-                  return stringifyRemoved(key);
-              }
-        });
-        return _.compact(lines).join('\n');
-    }; 
-    return iter(tree, '');
+  const iter = (properties, path) => {
+    const lines = properties
+      .flatMap((property) => {
+        const { key, after, before, type } = property;
+        const pathToProperty = [...path, key].join('.');
+        if (type === 'removed') {
+            return `Property '${pathToProperty}' was removed`;
+        }
+        if (type === 'updated') {
+            return `Property '${pathToProperty}' was updated. From ${stringify(before)} to ${stringify(after)}`;
+        }
+        if(type === 'changed') {
+            return iter(property.children, [...path, key]);
+        }
+        if(type === 'added') {
+            return `Property '${pathToProperty}' was added with value: ${stringify(after)}`;
+        }
+      });
+
+    return _.compact(lines).join('\n')
+  };
+
+  return iter(tree, []);
 };
 
 export default plain;
