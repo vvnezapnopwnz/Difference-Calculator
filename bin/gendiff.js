c88151d6310a11a7ef2794a525d5c34ce3b8dd51
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-10-03 14:46:15 +0600

    stage 8 ref

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information')
+  .helpOption('-h, --help', 'display help for command')
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('filepath1 filepath2')
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,16 +1,27 @@
 import * as fs from 'fs';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import path from 'path';
 import parse from './parsers.js';
 import compareFiles from './compareFiles.js';
 import formatterChooser from './formatters/index.js';
 
-export default (path1, path2, format) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  const readFile = (filename) => parse(fs.readFileSync(getFixturePath(filename), 'utf-8'), path.extname(filename));
-  const file1 = readFile(path1);
-  const file2 = readFile(path2);
+export default (path1, path2, format = 'stylish') => {
+  // const __filename = fileURLToPath(import.meta.url);
+  // const __dirname = dirname(__filename);
+  // const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  // const readFile = (filename) => parse(fs.readFileSync(getFixturePath(filename), 'utf-8'), path.extname(filename));
+  const extension1 = path.extname(path1).slice(1);
+  const extension2 = path.extname(path2).slice(1);
+
+  const readFile = (pathToFile) => {
+    const workingDir = process.cwd();
+    return parse(fs.readFileSync(path.resolve(workingDir, pathToFile), 'utf-8'));
+  };
+
+  const filepath1 = readFile(path1);
+  const filepath2 = readFile(path2);
+
+
+  const file1 = parse(filepath1, extension1);
+  const file2 = parse(filepath2, extension2);
   return formatterChooser(compareFiles(file1, file2), format);
 };
