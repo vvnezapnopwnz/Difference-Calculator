4ac658b65defd5fb61c83b58375521a7ae3422ed
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-10-03 15:57:57 +0600

    stage8 ref 3

--- src/parsers.js
+++ src/parsers.js
@@ -8,6 +8,12 @@ const parse = (data, extension) => {
       return yaml.load(data);
     case 'yml':
       return yaml.load(data);
+    case '.json':
+      return JSON.parse(data);
+    case '.yaml':
+      return yaml.load(data);
+    case '.yml':
+      return yaml.load(data);
     default:
       throw new Error(`Unknown type: ${extension}`);
   }
