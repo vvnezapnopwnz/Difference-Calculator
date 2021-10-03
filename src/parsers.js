e0b6a83eb9e174e5918e35f020a1bef05246fdbf
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-10-03 14:53:22 +0600

    stage 8-1

--- src/parsers.js
+++ src/parsers.js
@@ -2,10 +2,10 @@ import yaml from 'js-yaml';
 
 const parse = (data, extension) => {
   switch (extension) {
-    case '.json':
+    case 'json':
       return JSON.parse(data);
-    case '.yaml':
-    case '.yml':
+    case 'yaml':
+    case 'yml':
       return yaml.load(data);
     default:
       throw new Error(`Unknown type: ${extension}`);
