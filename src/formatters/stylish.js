4609d5a20a1d280a91065b355ada112dac8ecadc
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2021-10-07 20:26:45 +0600

    f ref3

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -10,27 +10,26 @@ const stringify = (data, indentSize) => {
   return result;
 };
 
-const stylish = (tree) => {
-  const iter = (nestedTree, indentSize = 0) => {
-    const lines = nestedTree.flatMap((node) => {
-      const {
-        type, key, after, before, children,
-      } = node;
-      if (type === 'added') {
-        return `${' '.repeat(indentSize + 2)}+ ${key}: ${stringify(after, indentSize + 4)}`;
-      } if (type === 'removed') {
-        return `${' '.repeat(indentSize + 2)}- ${key}: ${stringify(before, indentSize + 4)}`;
-      } if (type === 'nested') {
-        return `${' '.repeat(indentSize + 2)}  ${key}: ${iter(children, indentSize + 4)}`;
-      } if (type === 'unchanged') {
-        return `${' '.repeat(indentSize + 2)}  ${key}: ${stringify(before, indentSize + 4)}`;
-      }
-      return `${' '.repeat(indentSize + 2)}- ${key}: ${stringify(before, indentSize + 4)}\n${' '.repeat(indentSize + 2)}+ ${key}: ${stringify(after, indentSize + 4)}`;
-    });
-    const result = ['{', ...lines, `${' '.repeat(indentSize)}}`].join('\n');
-    return result;
-  };
-  return `${iter(tree)}`;
+const iter = (nestedTree, indentSize = 0) => {
+  const lines = nestedTree.flatMap((node) => {
+    const {
+      type, key, after, before, children,
+    } = node;
+    if (type === 'added') {
+      return `${' '.repeat(indentSize + 2)}+ ${key}: ${stringify(after, indentSize + 4)}`;
+    } if (type === 'removed') {
+      return `${' '.repeat(indentSize + 2)}- ${key}: ${stringify(before, indentSize + 4)}`;
+    } if (type === 'nested') {
+      return `${' '.repeat(indentSize + 2)}  ${key}: ${iter(children, indentSize + 4)}`;
+    } if (type === 'unchanged') {
+      return `${' '.repeat(indentSize + 2)}  ${key}: ${stringify(before, indentSize + 4)}`;
+    }
+    return `${' '.repeat(indentSize + 2)}- ${key}: ${stringify(before, indentSize + 4)}\n${' '.repeat(indentSize + 2)}+ ${key}: ${stringify(after, indentSize + 4)}`;
+  });
+  const result = ['{', ...lines, `${' '.repeat(indentSize)}}`].join('\n');
+  return result;
 };
+
+const stylish = (tree) => `${iter(tree)}`;
 
 export default stylish;
